33b82d2e1802b5a94dfd78aae944ea75d0930776
neffers <nefkrystal@gmail.com>
2017-02-10 12:54:35 -0500

    Removed need for user input of torrent groups, script now searches for them automatically. Improved user awareness of current actions

--- add-torrent-group.py
+++ add-torrent-group.py
@@ -9,6 +9,7 @@ transmission = transmissionrpc.Client()
 
 #set up requests session, log in to gazelle
 requestsSession = requests.Session()
+print("Logging in...")
 requestsSession.post(secret.baseurl+'login.php', data = {'username':secret.username, 'password':secret.password})
 
 #avoid spamming the server
@@ -16,47 +17,70 @@ time.sleep(2)
 
 #get user data json object, for passkey and authkey
 userdata = requestsSession.get(secret.baseurl+'ajax.php', params = {'action':'index'}).json()
+print("Getting auth and passkeys from server...")
 authkey = userdata['response']['authkey']
 passkey = userdata['response']['passkey']
 
-#get torrent group (album) id via userinput
-torrentGroupID = input("Input torrent group id: ")
-
-#set up json object of torrent group (album) data
-groupJSON = requestsSession.get(secret.baseurl+'ajax.php', params = {'action':'torrentgroup','id':torrentGroupID}).json()
-
-#wait 2 seconds between getting json and requesting torrent downloads
 time.sleep(2)
 
-#loop through torrents array to get each torrent id and use it to get a torrent file
-###!!!!USES secret.directory TO DETERMINE WHERE TORRENTS GET PLACED!!!!###
-for torrent in groupJSON['response']['torrents']:
-    #create infostring to present to user
-    torrentInfoString = "id "+str(torrent['id'])+", "+str(torrent['media'])+" rip in "+str(torrent['format'])+", encoding "+str(torrent['encoding'])
+#to manage looping through different pages
+done = False
+page = 1
+while not done:
 
-    print("found torrent "+torrentInfoString+", attempting to add to transmission...")
-    
-    #check if a torrent is marked as freeleech (as this is the purpose of the script)
-    freetorrent = torrent['freeTorrent']
-    if not freetorrent:
-        print("WARNING! Torrent id "+str(torrent['id'])+" NOT marked as freeleech! (It could be marked neutral-leech) Do you wish to continue downloading?")
-        validInput = False
-        while not validInput:
-            choice = input()
-            if choice.lower() == "y" or choice.lower() == "yes" or choice.lower() == "n" or choice.lower() == "no":
-                validInput = True
+    #use the search api to return the list of freetorrents
+    flsearch = requestsSession.get(secret.baseurl+'ajax.php', params = {'action':'browse','freetorrent':1,'page':page}).json()
+    print("Search request returned "+flsearch['status'])
+
+    #go through each group included in results
+    for torrentGroup in flsearch['response']['results']:
+
+        #get torrent group (album) id from the torrentgroup
+        torrentGroupID = torrentGroup['groupId']
+        groupInfoString = torrentGroup['groupName']+" by "+torrentGroup['artist']
+        print("Found group ID "+str(torrentGroupID)+", "+groupInfoString)
+
+        #set up json object of torrent group (album) data
+        groupJSON = requestsSession.get(secret.baseurl+'ajax.php', params = {'action':'torrentgroup','id':torrentGroupID}).json()
+
+        #wait 2 seconds between getting json and requesting torrent downloads
+        time.sleep(2)
+
+        #loop through torrents array to get each torrent id and use it to get a torrent file
+        ###!!!!USES secret.directory TO DETERMINE WHERE TORRENTS GET PLACED!!!!###
+        for torrent in groupJSON['response']['torrents']:
+            #create infostring to present to user
+            torrentInfoString = "id "+str(torrent['id'])+", "+str(torrent['media'])+" rip in "+str(torrent['format'])+", encoding "+str(torrent['encoding'])
+
+            print("Found torrent "+torrentInfoString)
+            
+            #check if a torrent is marked as freeleech (as this is the purpose of the script)
+            freetorrent = torrent['freeTorrent']
+            if not freetorrent:
+                print("WARNING! Torrent id "+str(torrent['id'])+" NOT marked as freeleech! (It could be marked neutral-leech) Do you wish to continue downloading?")
+                validInput = False
+                while not validInput:
+                    choice = input()
+                    if choice.lower() == "y" or choice.lower() == "yes" or choice.lower() == "n" or choice.lower() == "no":
+                        validInput = True
+                    else:
+                        print("Invalid input, try again")
+                if choice.lower() == "n" or choice.lower() == "no":
+                    continue
+
+            ##adding torrent can throw errors
+            try:
+                #adding torrent via add_torrent(url) wasn't working, i suspect because the tracker didn't like the request from the python library add_torrent uses to get from web
+                #so it uses requests (outside of the session), however the returned content wasn't working
+                #SO we encode it to base64 and then decode it
+                transmission.add_torrent(base64.b64encode(requests.get(secret.baseurl+'torrents.php', params = {'action':'download','id':torrent['id'],'authkey':authkey,'torrent_pass':passkey}).content).decode(), download_dir = secret.directory)
+            except transmissionrpc.TransmissionError:
+                print("torrent "+str(torrent['id'])+" failed (script threw transmission error, this can happen if you have downloaded that torrent too many times), continuing...")
             else:
-                print("Invalid input, try again")
-        if choice.lower() == "n" or choice.lower() == "no":
-            continue
-
-    ##adding torrent can throw errors
-    try:
-        #adding torrent via add_torrent(url) wasn't working, i suspect because the tracker didn't like the request from the python library add_torrent uses to get from web
-        #so it uses requests (outside of the session), however the returned content wasn't working
-        #SO we encode it to base64 and then decode it
-        transmission.add_torrent(base64.b64encode(requests.get(secret.baseurl+'torrents.php', params = {'action':'download','id':torrent['id'],'authkey':authkey,'torrent_pass':passkey}).content).decode(), download_dir = secret.directory)
-    except transmissionrpc.TransmissionError:
-        print("torrent "+str(torrent['id'])+" failed (script threw transmission error, this can happen if you have downloaded that torrent too many times), continuing...")
-    #wait 5 seconds before looping
-    time.sleep(5)
+                print("Successfully added to transmission!")
+            #wait 2 seconds before looping
+            time.sleep(2)
+    if page == flsearch['response']['pages']:
+        done = True
+    else:
+        page += 1
